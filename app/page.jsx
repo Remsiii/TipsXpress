24780238b9a1cd025af30f357b7228576b6b0178
Remsiii <remdab17@htl-kaindorf.at>
2023-06-18 14:07:37 +0200

    final tasks

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,12 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
-      Discover & Share
+    Win Big with Expert Tips
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> Bet Real Football</span>
+      <span className='orange_gradient text-center'> TipsExpress</span>
     </h1>
     <p className='desc text-center'>
-      Bet Real Football is a Footballbetting site where you get an football tipp for the next 
-      football game
+    Get the best soccer betting tips and increase your chances of winning. Join TipsXpress now!
     </p>
 
     <Feed />
