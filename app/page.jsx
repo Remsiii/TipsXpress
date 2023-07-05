d57f67daf29637df6603606b018ac623bd10db19
Remsiii <remdab17@htl-kaindorf.at>
2023-07-05 18:51:41 +0200

    Lining

--- app/page.jsx
+++ app/page.jsx
@@ -11,12 +11,12 @@ const Home = () => {
 
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
-        Win Big with Expert Tips
-      <br className='max-md:hidden' />
+        Win Big with Expert Tips 
+      <br />
       <span className='orange_gradient text-center'>PetaBet</span>
       </h1>
       <p className='mt-5 text-lg text-gray-300 sm:text-xl max-w-2xl text-center'>
-      Get the best soccer betting tips and increase your chances of winning. Join TipsXpress now!
+      Get the best soccer betting tips and increase your chances of winning. Join PetaBet now!
       </p>
       
   
